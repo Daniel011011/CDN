6c3ed4c1036f874a67b53f4311cc93e72f65a526
Daniel <89135196+Daniel011011@users.noreply.github.com>
2023-04-28 18:26:27 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -1,10 +1,28 @@
 from icalendar import Calendar
 import html
-from datetime import datetime, timedelta
 import os
+from datetime import datetime, timedelta
+from github import Github
+
 
 token = os.environ['MYTOKEN']
 
+repo_owner = "Daniel011011"
+repo_name = "CDN"
+
+#infile需要处理的文件
+infile_path = 'ics/课表.ics'
+infile_name = ''
+
+#输出位置
+outfile_path = "html/kebiao.html"
+outfile_name = "kebiao.html"
+
+#上传位置
+upfile_path = ''
+upfile_name = ''
+
+#打开文件
 def open_ics_file(filename):
     with open(filename, "rb") as f:
         data = f.read()
@@ -19,73 +37,82 @@ def extract_events(cal):
             event_dict["name"] = component.get("summary")
             event_dict["start_date"] = (component.get("dtstart").dt + timedelta(hours=8)).strftime("%Y-%m-%d")
             event_dict["start_time"] = (component.get("dtstart").dt + timedelta(hours=8)).strftime("%H:%M")
-            event_dict["description"] = component.get("description")
+            event_dict["LOCATION"] = component.get("LOCATION")
             events.append(event_dict)
     return events
-
-
+#写入html
 def display_events(events):
-    html_str = "<html><head><title>My Calendar</title></head><body>"
-    html_str += "<h1>My Calendar</h1>"
-    html_str += "<table border='1'>"
-    html_str += "<tr><th>Date</th><th>Name</th><th>Start Time</th><th>Description</th></tr>"
+    html_str = """<html>
+<html>
+    <head>
+        <title>My Calendar</title>
+        <style>
+          p {
+            font-size: 10vw; /* 字体大小为视口宽度的 5% */
+          }
+          th {
+            font-size: 45px; /* 设置表头的字体大小为 25px */
+        }
+        td {
+            font-size: 40px; /* 设置表格内容的字体大小为 15px */
+        }
+        </style>
+      </head>
+      
+<body>
+    <h1>My Calendar</h1>
+    <table border='2'>
+        <tr>
+            <th>日期</th>
+            <th>名称</th>
+            <th>时间</th>
+            <th>位置</th>
+        </tr>"""
     for event in events:
-        html_str += "<tr>"
-        html_str += f"<td>{html.escape(event['start_date'])}</td>"
-        html_str += f"<td>{html.escape(event['name'])}</td>"
-        html_str += f"<td>{html.escape(event['start_time'])}</td>"
-        html_str += f"<td>{html.escape(event['description'])}</td>"
-        html_str += "</tr>"
-    html_str += "</table></body></html>"
+        html_str += f"""
+        <tr>
+            <td>{html.escape(event['start_date'])}</td>
+            <td>{html.escape(event['name'])}</td>
+            <td>{html.escape(event['start_time'])}</td>
+            <td>{html.escape(event['LOCATION'])}</td>
+        </tr>"""
+    html_str += """
+    </table>
+</body>
+</html>"""
     return html_str
 
-def open_ics_file(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-        cal = Calendar.from_ical(data)
-        return cal
 
-cal = open_ics_file("ics/test.ics")
 
+#更新文件方法
+def update_github_file(file_path, file_name, token, repo_name):
+    # 创建 Github 对象，使用 token 或者用户名和密码进行认证
+    g = Github(token) # 或者 g = Github(login, password)
+
+    # 获取仓库对象
+    repo = g.get_user().get_repo(repo_name)
+
+    # 构造提交信息
+    commit_message = "update file by PyGithub"
+
+    # 获取文件内容
+    with open(file_path, "rb") as f:
+        file_content = f.read()
+
+    # 获取文件的 sha 值
+    contents = repo.get_contents(file_path)
+    sha = contents.sha
+
+    # 更新文件
+    repo.update_file(file_path, commit_message, file_content, sha)
+
+cal = open_ics_file(infile_path)
+print(cal)
 events = extract_events(cal)
 
 html_str = display_events(events)
-print(html_str.encode('utf-8').decode('utf-8'))
-with open('ics/output.html', 'w', encoding='utf-8') as f:
+
+with open(outfile_path, 'w', encoding='utf-8') as f:
     f.write(html_str)
 
-
-import base64
-import requests
-
-# 设置变量
-file_path = "ics/output.html"
-repo_owner = "Daniel011011"
-repo_name = "CDN"
-file_name = "output.html"
-commit_message = "upload file"
-
-# 读取文件内容，并进行 base64 编码
-with open(file_path, "rb") as f:
-    file_content = f.read()
-file_content_base64 = base64.b64encode(file_content).decode('utf-8')
-
-# 构造 HTTP 请求头部，包括认证信息和文件名
-headers = {
-    "Authorization": f"token {token}",
-    "Content-Type": "application/octet-stream",
-    "User-Agent": "Daniel011011",
-}
-
-# 构造 HTTP 请求参数
-params = {
-    "message": commit_message,
-    "content": file_content_base64,
-}
-
-# 发送 HTTP 请求
-api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
-response = requests.put(api_url, headers=headers, json=params)
-
-# 检查响应状态码，确保上传成功
-response.raise_for_status()
+update_github_file(outfile_path,0,token,repo_name)
